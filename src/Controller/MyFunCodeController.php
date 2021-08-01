b824472646e17cb3516eec35a222fa85ae5e9ed3
shamsuzzamansadi <abmsadi06@gmail.com>
2021-08-01 19:51:39 +0200

    [FEATURE] Unit testing added!

--- src/Controller/MyFunCodeController.php
+++ src/Controller/MyFunCodeController.php
@@ -9,24 +9,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class MyFunCodeController extends AbstractController
 {
     #[Route('/my/fun/code', name: 'my_fun_code')]
-    public function index(): Response
+    public function fizprocess()
     {   
-        foreach(range(1,100,1) as $numbers){
+        $numbers = range(1,100,1);
+        $results = array();
+        foreach(numbers as $number){
+            $outputValue = '';
 
-            if($numbers%3 == 0 && $numbers%5 == 0){
-                echo "FizzBuzz" ;
+            if($number%3 == 0 && $number%5 == 0){
+                $outputValue = "FizzBuzz" ;
             }elseif($numbers%3 == 0){
-                echo "Fizz";
+                $outputValue = "Fizz";
             }elseif($numbers%5 == 0){
-                echo "Buzz";
+                $outputValue = "Buzz";
             }else{
-                echo $numbers;
+                $outputValue = $number;
             }
-            echo "<br>";
+            $results[] = $outputValue;
         }
-        
+
         return $this->render('my_fun_code/index.html.twig', [
             'controller_name' => 'MyFunCodeController',
+            'results' => $results,
         ]);
     }
 }
